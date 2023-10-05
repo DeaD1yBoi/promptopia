5447b8fbc9971186c1f9dd72bfad0dcdc79f5b8b
DeaD1y <kulish233@Gmail.com>
2023-10-05 12:50:29 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+module.exports = { images: { domains: ['lh3.googleusercontent.com'], formats: ['image/avif', 'image/webp'], }, }
